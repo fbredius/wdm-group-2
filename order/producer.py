4fe0e6c7712d9320c5c291eba8e17d454e14199c
Bailey Tjiong <btjiong@outlook.com>
2022-06-15 11:09:49 +0200

    Some comments

--- order/producer.py
+++ order/producer.py
@@ -20,6 +20,10 @@ class Producer:
         self.queue = queue
 
     async def connect(self) -> "Producer":
+        """
+        Setup the connection with RabbitMQ, and start consuming for a reply
+        :return:
+        """
         self.connection = await connect("amqp://guest:guest@rabbitmq/", loop=self.loop)
         self.channel = await self.connection.channel()
         self.callback_queue = await self.channel.declare_queue(exclusive=True)
@@ -30,22 +34,36 @@ class Producer:
         await self.callback_queue.consume(self.on_response)
 
     async def on_response(self, message: AbstractIncomingMessage) -> None:
+        """
+        Sets the result of the Future if it receives a reply
+        :param message:
+        :return:
+        """
         async with message.process():
             if message.correlation_id is None:
                 print(f"Incorrect message {message!r}")
                 return
+
+            # Set the result of the Future
             future: asyncio.Future = self.futures.pop(message.correlation_id)
             future.set_result({"message": message.body.decode(), "status": message.type})
 
     async def publish(self, body, task=None, reply=False):
+        """
+        Sends a task to the corresponding queue
+        :param body:
+        :param task:
+        :param reply:
+        :return:
+        """
         correlation_id = str(uuid.uuid4())
         reply_queue = None
         # If a response is expected, set up the reply_to queue
         if reply:
             reply_queue = self.callback_queue.name
 
+        # A Future represents an eventual result of an asynchronous operation.
         future = self.loop.create_future()
-
         self.futures[correlation_id] = future
 
         await self.channel.default_exchange.publish(
@@ -58,6 +76,8 @@ class Producer:
             ),
             routing_key=self.queue
         )
+
+        # If an reply is expected, wait till the Future is ready
         if reply_queue is not None:
             response = await future
             return response
